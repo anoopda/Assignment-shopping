dd2acf96ca286244b19a7f6593a010006b73d9f7
anoopda <anoopda@qburst.com>
2016-08-16 19:42:41 +0530

    Shopping cart

---
+++ jsonfiles/productcat.js
@@ -0,0 +1,74 @@
+var product = [     		{ 	name:"candle",
+								id:305,
+								size:5,
+								color:"blue",
+								price:10
+							},
+							{
+
+								name:"candle",
+								id:306,
+								size:5,
+								color:"blue",
+								price:15
+							},
+							
+							{
+								name:"books",
+
+								id:400,
+								size:5,
+								color:"blue",
+								price:100	
+
+
+
+							}	,
+							{
+
+
+								name:"books",
+								id:401,
+								size:4,
+								color:"green",
+								price:250
+							},
+
+							{
+
+								name: "shoes",
+								id:500,
+								size:10,
+								color:"grey",
+								price:2500	
+
+
+
+							}
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var search_input= document.GetElementById("searchbar");
+
+// function sEarch {
+
+// var search = _.where(product, {name: search_input});
+// document.GetElementById("list").innerHTML= search;
+
+// }
+
---
+++ jsonfiles/shoplist.js
@@ -0,0 +1,50 @@
+var shoplist= [
+
+{
+
+	name : "candle",
+	qty  :    3, 
+
+},
+{
+
+name: "books",
+qty:  5,
+
+
+},
+{
+
+	name:"shoes",
+	qty:6,
+}
+
+
+
+
+];
+
+
+
+// var search_input= document.getElementById("searchbar");
+// function sEarch {
+
+// // var search = _.where(product, {name: search_input});
+// // document.GetElementById("list").innerHTML= search;
+// _.each(shop_list, function(num)
+
+// 		{ 
+
+// 		if  (product.nanme == num.name )
+
+// 			var itemlist =_.where(product, {name: search_input});
+
+
+// 		});
+
+// document.write(itemlist);
+// document.write(num);
+
+// }
+
+
